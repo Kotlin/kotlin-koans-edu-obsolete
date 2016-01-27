9eaffbfa7bb4c6959a6ee7bc9e09801bde521239
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2016-01-27 14:05:37 +0300

    Simplified zip util

--- src/koans/converter/zipUtil.kt
+++ src/koans/converter/zipUtil.kt
@@ -2,22 +2,20 @@ package koans.converter
 
 import java.io.File
 import java.nio.file.Files
-import java.nio.file.Path
 import java.nio.file.Paths
 import java.util.zip.ZipEntry
 import java.util.zip.ZipOutputStream
 
 fun packToZip(sourceDirPath: String, zipFilePath: String) {
     File(zipFilePath).let { if (it.exists()) it.delete() }
-    val p: Path = Files.createFile(Paths.get(zipFilePath))
 
-    ZipOutputStream(Files.newOutputStream(p)).use {
+    val zipFile = Files.createFile(Paths.get(zipFilePath))
+
+    ZipOutputStream(Files.newOutputStream(zipFile)).use {
         stream ->
-        val pp = Paths.get(sourceDirPath)
-        Files.walk(pp).filter { path -> !Files.isDirectory(path) }.forEach { path ->
-            val sp: String = path.toAbsolutePath().toString().replace(pp.toAbsolutePath().toString() + "/", "").replace(path.fileName.toString(), "")
-
-            val zipEntry = ZipEntry(sp + "/" + path.fileName.toString())
+        val sourceDir = Paths.get(sourceDirPath)
+        Files.walk(sourceDir).filter { path -> !Files.isDirectory(path) }.forEach { path ->
+            val zipEntry = ZipEntry(path.toString().substring(sourceDir.toString().length + 1))
 
             stream.putNextEntry(zipEntry)
             stream.write(Files.readAllBytes(path))
