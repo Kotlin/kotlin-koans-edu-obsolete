f90be9c9f8de77ce87a8868b8113fb138e0c1536
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2016-01-27 14:10:04 +0300

    Added task-ee.md  if task description differs for web-demo & educational edition plugin

--- src/koans/converter/strings.kt
+++ src/koans/converter/strings.kt
@@ -13,12 +13,13 @@ val COURSE_AUTHORS = listOf<String>() // todo should it be a list of strings?
 val MANIFEST_JSON = "manifest.json"
 val TASK_KT = "Task.kt"
 val TASK_MD = "task.md"
+val TASK_EE_MD = "task-ee.md"
 val SOLUTION_KT = "Solution.kt"
 val TEST_KT = "Test.kt"
+val CODE_FILE_EXTENSIONS = setOf(".kt", ".java")
 
 // new
 val COURSE_JSON = "course.json"
-val TASK_ANSWER_KT = "Task.answer.kt"
 val TESTS_KT = "tests.kt"
 val TASK_HTML = "task.html"
 val LESSON_DIR = "lesson"
--- src/koans/converter/tasksConverter.kt
+++ src/koans/converter/tasksConverter.kt
@@ -3,7 +3,6 @@ package koans.converter
 import org.pegdown.Extensions
 import org.pegdown.PegDownProcessor
 import java.io.File
-import java.nio.file.Files
 
 fun convertTasks(parentDir: File, course: Course, filesMap: FilesMap) {
     for ((lessonIndex, lesson) in course.lessons.withIndex()) {
@@ -16,28 +15,30 @@ fun convertTasks(parentDir: File, course: Course, filesMap: FilesMap) {
             val taskDir = lessonDir.subFile(taskDirName)
             taskDir.mkdir()
 
-            val taskInMD = filesMap[task].subFile(TASK_MD)
-            val taskInHTML = taskDir.subFile(TASK_HTML)
-            taskInHTML.writeText(convertMarkdownToHtml(taskInMD))
+            fun copyFileTaskAndTransform(oldFile: File, newFileName: String, addPackage: Boolean = true, transform: String.() -> String = { this }) {
+                val newFile = taskDir.subFile(newFileName)
+                newFile.writeText(oldFile.readText().transform().let { if (addPackage) it.addPackageName(lessonDirName, taskDirName) else it })
+            }
 
-            val oldTestFile = filesMap[task].subFile(TEST_KT)
-            val newTestFile = taskDir.subFile(TESTS_KT)
-            newTestFile.writeText(oldTestFile.readText().addPackageName(lessonDirName, taskDirName))
+            val taskDirectory = filesMap[task]
+
+            val taskInMD =
+                    taskDirectory.subFile(TASK_EE_MD).let { if (it.exists()) it else taskDirectory.subFile(TASK_MD) }
+            copyFileTaskAndTransform(taskInMD, TASK_HTML, addPackage = false) { convertMarkdownToHtml(this) }
+
+            copyFileTaskAndTransform(taskDirectory.subFile(TEST_KT), TESTS_KT)
 
             for ((name, taskFile) in task.task_files) {
-                val file = filesMap[taskFile]
-                val newTaskText = file.readText().let {
-                    if (taskFile.placeholders.isEmpty()) it else it.removeTaskWindowTags()
+
+                copyFileTaskAndTransform(filesMap[taskFile], filesMap[taskFile].name) {
+                    if (taskFile.placeholders.isEmpty()) this else removeTaskWindowTags()
                 }
-                val fileName = file.name.let { if (it == TASK_KT) TASK_ANSWER_KT else it }
-                taskDir.subFile(fileName).writeText(newTaskText.addPackageName(lessonDirName, taskDirName))
             }
         }
     }
 }
 
-fun convertMarkdownToHtml(file: File): String {
+fun convertMarkdownToHtml(markdownText: String): String {
     val processor = PegDownProcessor(Extensions.FENCED_CODE_BLOCKS)
-    val helpInMarkdown = String(Files.readAllBytes(file.toPath()))
-    return GFMNodeSerializer().toHtml(processor.parseMarkdown(helpInMarkdown.toCharArray()));
+    return GFMNodeSerializer().toHtml(processor.parseMarkdown(markdownText.toCharArray()));
 }
