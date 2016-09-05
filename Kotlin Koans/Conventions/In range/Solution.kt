d771d34c068f1cfc3888c807152898dbe74e0774
Guillermo Merino <gmerinojimenez@gmail.com>
2016-09-06 00:37:37 +0200

    start should be exclusive

--- Kotlin Koans/Conventions/In range/Solution.kt
+++ Kotlin Koans/Conventions/In range/Solution.kt
@@ -1,5 +1,5 @@
 <answer>class DateRange(val start: MyDate, val endInclusive: MyDate)<taskWindow> {
-    operator fun contains(item: MyDate): Boolean = start <= item && item <= endInclusive
+    operator fun contains(item: MyDate): Boolean = start < item && item <= endInclusive
 }</taskWindow></answer>
 
 fun checkInRange(date: MyDate, first: MyDate, last: MyDate): Boolean {
