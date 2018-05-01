b3e468a26e18e8d747789ea6336ccbc905b7fc2b
Edmund Johnson <edmund@jumpingmouse.uk>
2018-05-01 16:08:48 +0100

    Remove unnecessary toSet() conversion in Fold solution

--- Kotlin Koans/Collections/Fold/Solution.kt
+++ Kotlin Koans/Collections/Fold/Solution.kt
@@ -6,6 +6,6 @@ fun Shop.getSetOfProductsOrderedByEveryCustomer(): Set<Product> {
     /*<taskWindow>*/val allProducts = customers.flatMap { it.orders.flatMap { it.products }}.toSet()
     return customers.fold(allProducts, {
         orderedByAll, customer ->
-        orderedByAll.intersect(customer.orders.flatMap { it.products }.toSet())
+        orderedByAll.intersect(customer.orders.flatMap { it.products })
     })/*</taskWindow>*/
 }
