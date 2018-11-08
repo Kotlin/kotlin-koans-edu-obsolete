cfc4dcdd367a2cb92918b8de85f28517fb8d87e7
mybyte <fettesache@gmx.de>
2018-11-08 21:23:16 +0100

    Fix for Kotlin 1.3
    Doesn't compile in 1.3 because varargs (when used with named params) needs a collection.

--- Kotlin Koans/Collections/Compound tasks/Test.kt
+++ Kotlin Koans/Collections/Compound tasks/Test.kt
@@ -10,7 +10,7 @@ class K_Compound_Tasks {
     @Test fun testMostExpensiveDeliveredProduct() {
         val testShop = shop("test shop for 'most expensive delivered product'",
                 customer(lucas, Canberra,
-                        order(isDelivered = false, products = idea),
+                        order(isDelivered = false, products = *arrayOf(idea)),
                         order(reSharper)
                 )
         )
